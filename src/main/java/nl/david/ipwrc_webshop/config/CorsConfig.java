9738b2a489974da65aed6a8c5b44c68baab6fbfb
davidvdw02 <davidvdw02@gmail.com>
2024-01-30 18:01:42 +0100

    added cors solution

---
+++ src/main/java/nl/david/ipwrc_webshop/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package nl.david.ipwrc_webshop.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+   
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("Origin", "Content-Type", "Authorization")
+                .allowCredentials(true);
+    }
+}
