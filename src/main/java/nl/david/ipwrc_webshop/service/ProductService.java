39dfb49d7023c8b1fffd02d32c42343b557bef8a
davidvdw02 <davidvdw02@gmail.com>
2024-02-01 15:34:29 +0100

    cd pipeline test

--- src/main/java/nl/david/ipwrc_webshop/service/ProductService.java
+++ src/main/java/nl/david/ipwrc_webshop/service/ProductService.java
@@ -87,7 +87,7 @@ public class ProductService {
                 productRepository.deleteById(id);
             } catch (Exception e) {
                 e.printStackTrace();
-                //ci pileline test2
+                //ci pileline test1
             }
         }
     }
