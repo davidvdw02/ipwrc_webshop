2094e00a1b2bebf51348dd86f707c4f9cbacfcc9
davidvdw02 <davidvdw02@gmail.com>
2024-02-01 13:48:09 +0100

    cicd test

--- src/main/java/nl/david/ipwrc_webshop/service/ProductService.java
+++ src/main/java/nl/david/ipwrc_webshop/service/ProductService.java
@@ -87,6 +87,7 @@ public class ProductService {
                 productRepository.deleteById(id);
             } catch (Exception e) {
                 e.printStackTrace();
+                //ci pileline test
             }
         }
     }
